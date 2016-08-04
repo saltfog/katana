26b4d6e868eccee7854fa52a7bcd8478919be00f
Casey Jenkins <caseyjenkins@utah.gov>
2016-08-03 20:28:29 -0600

    Created own AppFunc method

--- katana/Program.cs
+++ katana/Program.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace kantana
 {
+    using System.Collections;
     using System.IO;
-    using AppFunc = Func<IDictionary<string, object>, Task>;
+    using AppFunc = Func<IDictionary<string, object>, Task>; //own using statement
 
     class Program
     {
@@ -32,47 +33,49 @@ namespace kantana
             {
                 Console.WriteLine("Requesting : " + environment.Request.Path);
 
-                await next();
-
                 Console.WriteLine("Response: " + environment.Response.StatusCode);
 
+                //foreach (var pair in environment.Environment)
+                //{
+                //    Console.WriteLine("{0}:{1}", pair.Key, pair.Value);
+                //}
+
+                await next();
             });
 
             //ConfigureWebApi(app);
 
-            app.UseWelcomePage();
-            app.UseHelloWorld();
+            //app.UseWelcomePage(); owin welcome page
+            app.UseTestComponent();
 
         }
     }
 
-
-    public static class AppBuilderExtenions
+    public static class AppBuildExtentions
     {
-        public static void UseHelloWorld(this IAppBuilder app)
+        public static void UseTestComponent(this IAppBuilder app)
         {
-            app.Use<HelloWorldComponent>();
+            app.Use<TestComponent>();
         }
     }
 
-
-
-    public class HelloWorldComponent
+    //Low Level Component
+    public class TestComponent
     {
+        //ctor
         AppFunc _next;
-        public HelloWorldComponent(AppFunc next)
+        public TestComponent(AppFunc next)
         {
             _next = next;
-        }
 
-        public Task Invoke(IDictionary<string, object> environment)
+        }
+        public async Task Invoke(IDictionary<string, object> environment)
         {
             var response = environment["owin.ResponseBody"] as Stream;
             using (var writer = new StreamWriter(response))
             {
-                return writer.WriteAsync("Hello!!");
+                await writer.WriteAsync("MSP UDOH");
             }
-
         }
     }
 }
